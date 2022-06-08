14e1360ef4ccc05a7d97befa5637ab5b3e99dfc9
Juan Alice <alice.juan@student-cs.fr>
2022-06-08 17:58:09 +0200

    ajout du component genres

---
+++ frontend/src/components/Genres.vue
@@ -0,0 +1,30 @@
+<template>
+    <p>
+        <ul id="genres">
+            <li v-for="genre in genres" :key="genre.name">
+                {{ genre.name }}
+            </li>
+        </ul>
+    </p>
+  
+</template>
+
+<script>
+export default {
+  name: "Genres",
+  url_img: "",
+  props: {
+    genres: Array,
+  },
+};
+</script>
+
+<style scoped>
+li {
+  line-height: 1.5;
+}
+
+/* p{
+    margin: 0 auto;
+} */
+</style>
