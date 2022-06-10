c98113325501f53ece5665f3ced4811325efedb6
Juan Alice <alice.juan@student-cs.fr>
2022-06-10 10:09:19 +0200

    get scores fini

--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const ScoreModel = require("../models/scores");
+const MovieModel = require("../models/movies");
 module.exports = router;
 const id_user = "629f485825c978cfea8b715b";
 
@@ -17,10 +18,31 @@ router.get("/", async function (req, res) {
 
     const scores_list = [];
     for (const s of score) {
-      scores_list.push([s.score, s.id_movie]);
+      scores_list.push({ score: s.score, id_movie: s.id_movie });
     }
     console.log("score : " + scores_list);
-    res.json(scores_list);
+    // var mylist = [
+    //   [1, "c"],
+    //   [3, "a"],
+    //   [5, "b"],
+    // ];
+    const orderedlist = scores_list.sort(function (a, b) {
+      return a.score > b.score ? -1 : a.score == b.score ? 0 : 1;
+    });
+    let best_movies = [];
+    let count = 0;
+    for (const movie of orderedlist) {
+      if (count < 20) {
+        const themoviedb = await MovieModel.findOne({ _id: movie.id_movie });
+        best_movies.push(themoviedb);
+        count = count + 1;
+      }
+    }
+    // const orderedlist = scores_list.sort(function (a, b) {
+    //   return a[1].localeCompare(b[1]);
+    // });
+    // console.log("score ordonné : " + best_movies);
+    res.json(best_movies);
   } catch (error) {
     console.log(error);
     res.send("nok");
